a513ec1fd0da49e1cab9158edaa7e1cab70c90a2
Haikel BEN HMIDA <benhmida.haikel@gmail.com>
2023-04-07 13:25:41 +0100

    Create home.js

---
+++ routes/home.js
@@ -0,0 +1,17 @@
+// File: routes/home.js
+
+const express = require('express');
+const router = express.Router();
+
+const { getEmployeeRecords } = require('../controllers/employeeController');
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  // Get the employee records
+  const records = await getEmployeeRecords();
+
+  // Render the home page with the records
+  res.render('home', { title: 'Employee Attendance Tracker', records });
+});
+
+module.exports = router;
