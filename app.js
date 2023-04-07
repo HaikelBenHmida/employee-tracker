ac7f306a5aff0a606ce108f86c71dfc57fbb914d
Haikel BEN HMIDA <benhmida.haikel@gmail.com>
2023-04-07 09:06:01 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const mongoose = require('mongoose');
+
+const homeRouter = require('./routes/home');
+const adminRouter = require('./routes/admin');
+const employeeRouter = require('./routes/employee');
+
+const app = express();
+
+// Connect to database
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/employee-tracker', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+// Setup view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'secret',
+    resave: false,
+    saveUninitialized: true
+}));
+
+// Routes
+app.use('/', homeRouter);
+app.use('/admin', adminRouter);
+app.use('/employee', employeeRouter);
+
+// Error handling
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function(err, req, res, next) {
+    console.error(err);
+
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: app.get('env') === 'development' ? err : {}
+    });
+});
+
+module.exports = app;
